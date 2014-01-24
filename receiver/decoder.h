672849223ff5966c5f4fd63b1128cc15a5d16f75
ChrisMicro <christoph@roboterclub-freiburg.de>
2014-01-24 15:29:52 +0100

    decoder swiched back to pin input

--- receiver/decoder.h
+++ receiver/decoder.h
@@ -29,7 +29,7 @@ extern "C"
 	// external capacitor
 	// ADC-sampling is slower than direct
 	// digital pin sampling
-	#define INPUTFROMADC
+	//#define INPUTFROMADC
 
 	#define DEBUGLED 5
 	//#define DEBUGLED 4
